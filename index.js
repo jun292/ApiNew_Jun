0e96b88cad2229726f4bac0f5076847e217485d9
jun292 <akunsecondgh@gmail.com>
2025-06-26 20:11:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
         if (data && typeof data === 'object') {
             const responseData = {
                 status: data.status,
-                creator: settings.apiSettings.creator || "Created Using Rynn UI",
+                creator: settings.apiSettings.creator || "Created Using JunE UI",
                 ...data
             };
             return originalJson.call(this, responseData);
